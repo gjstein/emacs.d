161a1b5fcecf8e5a652cdcd5627dea0391b51088
gjstein <gregory.j.stein@gmail.com>
2015-08-22 23:17:20 -0400

    updated emmet config

--- config/coding.el
+++ config/coding.el
@@ -66,6 +66,9 @@
   :ensure t
   :defer t
   :diminish emmet-mode
+  :init
+  (add-hook 'sgml-mode-hook 'emmet-mode) ;; Auto-start on any markup modes
+  (add-hook 'css-mode-hook  'emmet-mode) ;; enable Emmet's css abbreviation.
   )
 
 ;; === Syntax Highlighting ===
