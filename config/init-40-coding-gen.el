1fed8f4443598ca519d29e5cf3f062ec0dcdab1d
gjstein <gregory.j.stein@gmail.com>
2015-11-06 15:44:03 -0500

    Added 'word-diff' to magit congif

--- config/init-40-coding-gen.el
+++ config/init-40-coding-gen.el
@@ -59,6 +59,9 @@
   :ensure t
   :defer t
   :bind ("C-x g" . magit-status)
+  :init
+  (setq magit-diff-options (quote ("--word-diff")))
+  (setq magit-diff-refine-hunk 'all)
   )
 
 ;; == flycheck ==
