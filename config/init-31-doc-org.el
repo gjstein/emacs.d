2caf67cb65e2ef8f2f54f81f397a58c6aa395865
gjstein <gregory.j.stein@gmail.com>
2016-12-17 18:47:40 -0500

    Clean org config and add some evil bindings

--- config/init-31-doc-org.el
+++ config/init-31-doc-org.el
@@ -29,16 +29,14 @@
 	 ("\C-c c" . org-capture))
   :config
   ;; Custom functions for emacs & org mode
-  ;; (load-file "~/.emacs.d/config/bh-org.el")
   (load-file "~/.emacs.d/config/gs-org.el")
+  (require 'org)
 
-  ;; Agenda
+  ;; == Agenda ==
+  (defvar org-agenda-window-setup)
   (setq org-agenda-window-setup 'current-window)
-  ;;(setq org-agenda-files (quote ("~/org" "~/rrg/gjstein_notebook")))
-
-  (define-key global-map "\C-cl" 'org-store-link)
   
-  ; Run/highlight code using babel in org-mode
+  ;; Run/highlight code using babel in org-mode
   (org-babel-do-load-languages
    'org-babel-load-languages
    '(
@@ -48,7 +46,34 @@
      ))
   ;; Syntax hilight in #+begin_src blocks
   (setq org-src-fontify-natively t)
-  
+
+  ;; Evil key configurations (agenda)
+  (evil-set-initial-state 'org-agenda-mode 'normal)
+  (defvar org-agenda-mode-map)
+  (evil-define-key 'normal org-agenda-mode-map
+    "l" 'org-agenda-later
+    "h" 'org-agenda-earlier
+    "j" 'org-agenda-next-line
+    "k" 'org-agenda-previous-line
+    (kbd "RET") 'org-agenda-switch-to
+    [escape] 'org-agenda-quit
+    "q" 'org-agenda-quit
+    "s" 'org-agenda-save-all-org-buffers
+    "t" 'org-agenda-todo
+    (kbd "SPC") 'org-agenda-show-and-scroll-up
+    )
+  (evil-leader/set-key-for-mode 'org-agenda-mode
+    "i" 'org-agenda-clock-in
+    "o" 'org-agenda-clock-out
+    "k" 'org-agenda-kill
+    "/" 'org-agenda-filter-by-tag
+    )
+
+  ;; Evil key configuration (org)
+  (evil-leader/set-key-for-mode 'org-mode
+    "i" 'org-clock-in
+    "o" 'org-clock-out
+    )
   )
 
 ;;; init-31-doc-org.el ends here
