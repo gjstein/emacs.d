2cef9ddb6f01f0950d907a6e9b261654d234c8ad
gjstein <gregory.j.stein@gmail.com>
2018-07-13 11:08:55 -0700

    update a bunch of org/agenda stuff

--- config/gs-org.el
+++ config/gs-org.el
@@ -6,7 +6,7 @@
 (setq org-directory "~/org")
 (setq org-default-notes-file "~/org/refile.org")
 (defvar org-default-diary-file "~/org/diary.org")
-(setq org-agenda-files (quote ("~/org")))
+(setq org-agenda-files (quote ("~/org" "~/org/archive")))
 
 ;; Display properties
 (setq org-cycle-separator-lines 0)
@@ -54,17 +54,18 @@ Callers of this function already widen the buffer view."
           nil
         t))))
 
-;; == Tags ==
-(setq org-tag-alist '((:startgroup)
-		      ("@errand" . ?e)
-		      ("@campus" . ?c)
-		      ("@home" . ?h)
-		      (:endgroup)
-		      ("WAITING" . ?w)
-		      ("PERSONAL" . ?P)
-		      ("RRG" . ?W)
-		      ("NOTE" . ?n)
-		      ))
+;; ;; == Tags ==
+;; (setq org-tag-alist '((:startgroup)
+;; 		      ("@errand" . ?e)
+;; 		      ("@campus" . ?c)
+;; 		      ("@home" . ?h)
+;; 		      (:endgroup)
+;; 		      ("WAITING" . ?w)
+;; 		      ("PERSONAL" . ?P)
+;; 		      ("RRG" . ?W)
+;; 		      ("NOTE" . ?n)
+;; 		      ("AR" . ?a)
+;; 		      ))
 
 ;; Allow setting single tags without the menu
 (setq org-fast-tag-selection-single-key 'expert)
@@ -76,7 +77,7 @@ Callers of this function already widen the buffer view."
 (setq org-use-fast-todo-selection t)
 (setq org-todo-keywords
       '((sequence "TODO(t)" "NEXT(n)" "|" "DONE(d)")
-	(sequence "WAITING(w@/!)" "INACTIVE(i@/!)" "|" "CANCELLED(c@/!)" "MEETING")))
+	(sequence "WAITING(w@/!)" "INACTIVE(i)" "|" "CANCELLED(c@/!)" "MEETING")))
 ;; Custom colors for the keywords
 (setq org-todo-keyword-faces
       '(("TODO" :foreground "red" :weight bold)
@@ -119,7 +120,7 @@ Callers of this function already widen the buffer view."
 	 ("b" "Blank" entry (file org-default-notes-file)
 	  "* %?\n%u")
 	 ("m" "Meeting" entry (file org-default-notes-file)
-	  "* MEETING with %? :MEETING:\n%t" :clock-in t :clock-resume t)
+	  "* MEETING with %? :MEETING:\n" :clock-in t :clock-resume t)
 	 ("d" "Diary" entry (file+datetree "~/org/diary.org")
 	  "* %?\n%U\n" :clock-in t :clock-resume t)
 	 ("D" "Daily Log" entry (file "~/org/daily-log.org")
@@ -147,11 +148,82 @@ Callers of this function already widen the buffer view."
 
 ;; == Habits ==
 (require 'org-habit)
-
-(setq org-modules '(org-habit))
+(add-to-list 'org-modules 'org-habit)
+(setq org-habit-graph-column 48)
 (setq org-habit-show-habits-only-for-today t)
 
-;; == Clocking Functions ==
+;; == Checklists ==
+(require 'org-checklist)
+
+;; == Org-ID ==
+(require 'org-id)
+;; I might also need org-ref
+
+;; == Contacts ==
+(require 'org-contacts)
+
+(defun gs-store-org-headline ()
+  (interactive)
+  (when (and (eq major-mode 'org-mode)
+             (org-at-heading-p))
+    (org-store-link-props
+     :type "file"
+     :link (format "file:*%s" (nth 4 (org-heading-components)))
+     :description (nth 4 (org-heading-components)))))
+
+(defun gstest ()
+  (interactive)
+  	   ;; Just link to current headline
+	   (setq cpltxt (concat "file:"
+				(abbreviate-file-name
+				 (buffer-file-name (buffer-base-buffer)))))
+	   ;; Add a context search string
+	   (when t
+	     (let* ((element (org-element-at-point))
+		    (name (org-element-property :name element)))
+	       (setq txt (cond
+			  ((org-at-heading-p) nil)
+			  (name)
+			  ((org-region-active-p)
+			   (buffer-substring (region-beginning) (region-end)))))
+	       (when (or (null txt) (string-match "\\S-" txt))
+		 (setq cpltxt
+		       (concat cpltxt "::"
+			       (condition-case nil
+				   (org-make-org-heading-search-string txt)
+				 (error "")))
+		       desc (or name
+				(nth 4 (ignore-errors (org-heading-components)))
+				"NONE")))))
+	   (when (string-match "::\\'" cpltxt)
+	     (setq cpltxt (substring cpltxt 0 -2)))
+	   (setq link cpltxt)
+	   link
+	   )
+
+
+
+(defun gs-helm-org-link-to-contact ()
+  (interactive)
+  (if (eq major-mode 'org-mode)
+      (let ((temp-point (point))
+	    (temp-buffer (current-buffer))
+	    (org-refile-targets (quote (("~/org/contacts.org" :level . 2))))
+	   ;; (org-refile-targets (quote ((("~/org/contacts.org")) :maxlevel . 9)))
+	    )
+	(org-refile '(4))
+	(let ((link-text (gstest))
+	      (desc-text (nth 4 (org-heading-components))))
+	       ;(concat "[[file:contacts.org::" (nth 4 (org-heading-components)) "]]")))
+	  (unless (eq (current-buffer) temp-buffer) (switch-to-buffer temp-buffer))
+	  (goto-char temp-point)
+	  (insert (concat "[[" link-text "][" desc-text "]]")
+	  )
+	))
+    (user-error "This function is meant to be called within org")
+    ))
+
+;; == clocking Functions ==
 (require 'org-clock)
 
 ;; If not a project, clocking-in changes TODO to NEXT
@@ -188,9 +260,10 @@ Switch projects and subprojects from NEXT back to TODO"
 
 ;; Set the times to display in the time grid
 (setq org-agenda-time-grid
-  '((daily today require-timed)
-    "----------------"
-    (800 1200 1600 2000)))
+      (quote
+       ((daily today remove-match)
+        (800 1200 1600 2000)
+        "......" "----------------")))
 
 ;; Some helper functions for selection within agenda views
 (defun gs/select-with-tag-function (select-fun-p)
