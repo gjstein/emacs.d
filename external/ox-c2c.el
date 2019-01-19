eda765e1e5eb2a82df0d9332b4afc0c5678b44fe
Gregory Stein <gjstein@dhcp-18-30-16-88.dyn.mit.edu>
2019-01-18 22:38:19 -0500

    lots of changes necessary to export html bib from org

--- external/ox-c2c.el
+++ external/ox-c2c.el
@@ -24,12 +24,88 @@
     )
   )
 
+
+(defun gjs-redef-reftex-get-bib-names ()
+  (defun reftex-get-bib-names (field entry)
+    "Return a list with the author or editor names in ENTRY.
+If FIELD is empty try \"editor\" field."
+    (let ((names (reftex-get-bib-field field entry)))
+      (if (equal "" names)
+	  (setq names (reftex-get-bib-field "editor" entry)))
+					; Replace newline characters
+      (while (string-match "\n" names)
+	(setq names (replace-match " " nil t names)))
+					; Replace duplicate spaces
+      (while (string-match "  " names)
+	(setq names (replace-match " " nil t names)))
+      (while (string-match "[{}]" names)
+	(setq names (replace-match "" nil t names)))
+      (while (string-match "\\band\\b[ \t]*" names)
+	(setq names (replace-match "\n" nil t names)))
+      (while (string-match "\\(.*\\), \\(.*\\)" names)
+	(setq names (replace-match "\\2 \\1" nil nil names)))
+					; (while (string-match "[\\.a-zA-Z\\-]+\\.[ \t]*\\|,.*\\|[{}]+" names)
+					;(setq names (replace-match "" nil t names)))
+      (while (string-match "^[ \t]+\\|[ \t]+$" names)
+	(setq names (replace-match "" nil t names)))
+      (while (string-match "[ \t][ \t]+" names)
+	(setq names (replace-match " " nil t names)))
+      (while (string-match "[ \n][ \n]+" names)
+	(setq names (replace-match " " nil t names)))
+      (split-string names "\n")))
+  )
+
 (defun org-c2c-run-pandoc (filename output-format &optional options)
   (let* ((args (list "-f" "org"
 		     "-t" (symbol-name output-format)
                      options
                      filename))
-         (command (concat org-pandoc-command " " (mapconcat 'identity args " "))))
+         (command (concat "pandoc " (mapconcat 'identity args " "))))
     (shell-command-to-string command)))
+
+(defun gjs-org-ref-get-bibtex-entry-html-no-key (key)
+  "Return an html string for the bibliography entry corresponding to KEY."
+  (let ((output))
+    (setq output (org-ref-get-bibtex-entry-citation key))
+    (setq output (gjs-org-ref-clean-unused-entry-html output))
+    (format "<li id=\"%s\">%s</li>"
+	    key output)))
+
+(defun gjs-org-ref-clean-unused-entry-html (entry-html)
+  "Return from the html string ENTRY-HTML a cleaner version"
+  ;; remove any stray "\url" commands
+  (setq entry-html (replace-regexp-in-string "\\\\url" "" entry-html))
+  ;; unescape the &
+  (setq entry-html (replace-regexp-in-string "\\\\&" "&" entry-html))
+  ;; hack to remove unnecessary \it
+  (setq entry-html (replace-regexp-in-string "\\\\it " "" entry-html))
+  ;; hack to remove duplicate spaces
+  (setq entry-html (replace-regexp-in-string "  " " " entry-html))
+  ;; hack to replace {} around text
+  (setq entry-html (replace-regexp-in-string "{" "" entry-html))
+  (setq entry-html (replace-regexp-in-string "}" "" entry-html))
+  ;; get rid of empty parens
+  (setq entry-html (replace-regexp-in-string "()" "" entry-html))
+  ;; Remove empty volume, number field if empty
+  (setq entry-html (replace-regexp-in-string "<b></b>," "" entry-html))
+  ;; get rid of empty link and doi
+  (setq entry-html (replace-regexp-in-string " <a href=\"\">link</a>\\." "" entry-html))
+  ;; change double dash to single dash
+  (setq entry-html (replace-regexp-in-string "--" "-" entry-html))
+  (setq entry-html (replace-regexp-in-string " <a href=\"http://dx\\.doi\\.org/\">doi</a>\\." "" entry-html))
+  entry-html)
+
+(defun gjs-rm-cite-prefix (&optional key)
+  (replace-regexp-in-string "cite.?:" "" key))
+
+(defun gjs-c2c-bibliography-no-keys (&optional nocite-keys)
+  (gjs-redef-reftex-get-bib-names)
+  (let ((keys (append (org-ref-get-bibtex-keys t)
+		      (mapcar 'gjs-rm-cite-prefix nocite-keys))))
+    (when keys
+      (concat "#+BEGIN_HTML\n<ul>\n"
+	      (mapconcat (lambda (x) (gjs-org-ref-get-bibtex-entry-html-no-key x)) keys "\n")
+	      "\n</ul>\n#+END_HTML"))))
+
 
 (provide 'ox-c2c)
