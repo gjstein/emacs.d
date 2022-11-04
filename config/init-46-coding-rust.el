2bddaf9a46a725e913b6e77f6582ba17951fb58b
Gregory Stein <gjstein@VSE-N01651.local>
2022-11-04 09:44:41 -0400

    update some rust configuration options + bindings

--- config/init-46-coding-rust.el
+++ config/init-46-coding-rust.el
@@ -18,6 +18,22 @@
   :ensure t
   :defer t
   :init
+  (general-define-key
+   :states '(normal motion)
+   :prefix gjs-leader-key
+   "ct" 'cargo-process-test
+   "cb" 'cargo-process-build
+   "cr" 'cargo-process-run
+   "ce" 'cargo-process-bench
+   (kbd "c RET") 'cargo-process-fmt
+   )
+  )
+
+(use-package cargo
+  :ensure t
+  :after rust-mode
+  :init
+  (add-hook 'rust-mode-hook 'cargo-minor-mode)
   )
 
 (use-package flycheck-rust
